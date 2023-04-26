3cfd7ae603bab408e7a06245b9ada1aec371225a
Kadson Lima <kadson-kaio@hotmail.com>
2023-04-26 11:34:56 -0300

    Atualizar o wsocket.js
    add serve API websocket

--- src/utils/wsocket.js
+++ src/utils/wsocket.js
@@ -1,6 +1,8 @@
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000'); // substitua o URL pelo endereço do seu servidor Socket.io
+  const SERVE_API = import.meta.env.VITE_SERVER_API;
+
+const socket = io(SERVE_API); // substitua o URL pelo endereço do seu servidor Socket.io
 
 socket.on('connect', () => {
   console.log(`Conexão estabelecida com sucesso!`);
