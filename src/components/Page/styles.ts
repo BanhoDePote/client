1f4d01200c51b71928c8f839a0312f8a64dadef6
Kadson Lima <kadson-kaio@hotmail.com>
2023-05-22 12:20:41 -0300

    styles: responsive loading

--- src/components/Page/styles.ts
+++ src/components/Page/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 25px 25px;
+  padding: 10px 10px;
   margin-top: 92px;
-  min-height: 100vh;
+  height: calc(100vh - 92px);
 `
